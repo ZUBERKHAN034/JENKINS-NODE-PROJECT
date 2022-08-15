00f8e558b373e7bc4c3032719472e510910c63e2
Zuber Khan <50729008+ZUBERKHAN034@users.noreply.github.com>
2022-08-15 16:34:29 +0530

    testing jenkins

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/',(req,res) => res.send('Hello Jenkins test'));
+app.get('/',(req,res) => res.send('Hello Jenkins From Github'));
 
 app.listen(PORT,() => console.log('Server is running on port',PORT));
